f79fd6f540ef3ef1bbaf6ef258e5d41c99749819
aws-nasr <awsdweikat2019@gmail.com>
2024-12-23 09:27:00 +0200

    addWhyNaturalStoreAComp

---
+++ natural-store/src/components/WhyNaturalStoreA/A.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './A.css';
+
+const WhyNaturalStoreA = () => {
+
+return (
+    <div className="Why-natural-store">
+        <header>WHY NATURAL STORE?</header>
+        <p className="p-1">At Natural Store, we believe in the power of nature to nurture your well-being. Our mission is to provide you with high-quality, eco-friendly products that promote a healthier lifestyle while caring for the planet. Every item in our store is carefully selected from trusted suppliers who share our commitment to sustainability and ethical practices. Whether you're looking for organic skincare, natural supplements, or eco-conscious household items, you'll find products that align with your values. We prioritize transparency, ensuring you know exactly what's in each product and how it benefits you. Shopping with us means supporting small businesses, reducing your carbon footprint, and embracing a more mindful way of living. Experience the difference with Natural Store – where wellness meets sustainability, and every purchase makes a positive impact.</p>
+        <img alt="This is a picture of one of the creams" src="/1.png" className="img-1"/>
+        <img alt="This picture is of a person applying cream to his face" src="/2.png" className="img-2"/>
+        <img alt="This picture is of a person applying cream to his face" src="/2.png" className="img-3"/>
+    </div>
+
+);
+
+};
+
+
+export default WhyNaturalStoreA; 
