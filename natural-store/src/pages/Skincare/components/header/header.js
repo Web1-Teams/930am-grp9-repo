1cad65b5b1409f851c5088afc008ed3974e59fec
ahmadsawalha11 <sawalhaahmad733@gmail.com>
2025-01-01 18:03:27 +0200

    added Skincare page componets

---
+++ natural-store/src/pages/Skincare/components/header/header.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import './header.css';
+
+const Header = () => {
+
+return(
+
+    <header className="header">
+
+        <br/>
+        <div className="nav-trail">
+            <a className="nav-trail-1" href="Home.html">Home </a><b> Supplements</b>
+            <hr/>
+        </div>
+
+        <div className="hero-section">
+            <img className="Skincare-img" src="Skincare.png" alt="Skincare"/>
+            <img className="img0" src="/omega-3 fish oil.png" alt="pill_jar_mockup"/>
+        </div>
+
+    </header>
+)
+}
+
+export default Header;
